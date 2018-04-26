a1faec1b5f3ebb4c15719ba274219bca44b8925f
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-04-26 00:59:04 -0300

    Início da home similar ao app do Banco do Brasil

--- lib/ui/home/HomePage.dart
+++ lib/ui/home/HomePage.dart
@@ -3,13 +3,27 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ListView(
-      children: <Widget>[
-        new Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-          child: new Text('Home'),
-        ),
-      ]
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 100.0),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          new Column(
+            children: <Widget>[
+              new Text('Saldo'),
+              new Text('R\$ 200,00', style: new TextStyle(
+                fontWeight: FontWeight.bold
+              ),),
+            ],
+          ),
+          new Column(
+            children: <Widget>[
+              new Text('Lançamentos futuros'),
+              new Text('Lançamentos futuros'),
+            ],
+          ),
+        ],
+      )
     );
   }
 }
