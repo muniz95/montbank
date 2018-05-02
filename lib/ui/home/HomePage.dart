aa254363d0a784438962e1733b064a910ea95ccc
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-05-01 23:09:14 -0300

    Todos os menus adicionados

--- lib/ui/home/HomePage.dart
+++ lib/ui/home/HomePage.dart
@@ -11,15 +11,60 @@ class HomePage extends StatelessWidget {
           new Column(
             children: <Widget>[
               new Text('Saldo'),
-              new Text('R\$ 200,00', style: new TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
+              new FlatButton(
+                child: new Text(
+                  'R\$ 200,00',
+                  style: new TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                onPressed: () {
+                  showDialog(context: context, builder: (BuildContext context) {
+                    return new AlertDialog(
+                      title: new Text('Saldos'),
+                      content: new Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          new Text('Saldo: R\$: 200,00'),
+                          new Text('Limite: R\$: 100,00'),
+                        ],
+                      )
+                    );
+                  }
+                );
+              }),
             ],
           ),
           new Column(
             children: <Widget>[
               new Text('Lançamentos futuros'),
-              new Text('Lançamentos futuros'),
+              new FlatButton(
+                child: new Text(
+                  'R\$ 200,00',
+                  style: new TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                onPressed: () {
+                  showDialog(context: context, builder: (BuildContext context) {
+                    return new AlertDialog(
+                      title: new Column(
+                        children: <Widget>[
+                          new Text('Lançamentos futuros')
+                        ],
+                      ),
+                      content: new Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          new Text('21/01/17: R\$: 200,00'),
+                          new Text('21/01/17: R\$: 200,00'),
+                          new Text('21/01/17: R\$: 200,00'),
+                        ],
+                      ),
+                    );
+                  }
+                );
+              }),
             ],
           ),
         ],
